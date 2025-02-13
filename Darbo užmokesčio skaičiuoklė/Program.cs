775a27175f37c1c5325dfef4be6d515728d94338
Edvinas Jonauskas <jonauskas.edvinas0@gmail.com>
2025-02-13 14:57:05 +0200

    Add project files.

---
+++ Darbo užmokesčio skaičiuoklė/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Iveskite dirbtu valandu skaiciu per savaite: ");
+double valandos = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Iveskite valandini atlyginima: ");
+double atlyginimas = double.Parse(Console.ReadLine());
+
+double savaitesPelnas = valandos * atlyginimas;
+double menesioPelnas = savaitesPelnas * 4;
+
+Console.WriteLine("Savaites uzdarbis: " + savaitesPelnas + " $");
+Console.WriteLine("Menesio uzdarbis: " + menesioPelnas + " $");
